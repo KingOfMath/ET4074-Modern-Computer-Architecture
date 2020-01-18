13e21bb095afe663a67991aff05bd32910461965
KingOfMath <823221784@qq.com>
2020-01-18 02:23:10 +0100

    Add files via upload

---
+++ lab2/src/benchmarks.h
@@ -0,0 +1,17 @@
+
+#ifndef _BENCHMARKS_H_
+#define _BENCHMARKS_H_
+
+// Runs the convolution_3x3 benchmark.
+int main_convolution_3x3(void);
+
+// Runs the engine benchmark.
+int main_engine(void);
+
+// Runs the fir benchmark.
+int main_fir(void);
+
+// Runs the adpcm benchmark.
+int main_adpcm(void);
+
+#endif
